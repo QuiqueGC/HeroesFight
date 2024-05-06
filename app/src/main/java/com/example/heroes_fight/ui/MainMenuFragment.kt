a2b7e1bb2abcf8a410a2af03ddfcd97142a44492
Quique <enriquegcameselle@gmail.com>
2024-05-06 19:47:04 +0200

    Added MainMenuFragment.kt and its xml

---
+++ app/src/main/java/com/example/heroes_fight/ui/MainMenuFragment.kt
@@ -0,0 +1,18 @@
+package com.example.heroes_fight.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.heroes_fight.R
+
+class MainMenuFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main_menu, container, false)
+    }
+}
