92ef17f3b4af13f5f30e9ee4fc292ae16463a363
Quique <enriquegcameselle@gmail.com>
2024-05-07 18:44:23 +0200

    added RemoteApiServices.kt with all the calls (the body of the responses is Any in all of it right now)

--- app/src/main/java/com/example/heroes_fight/data/domain/repository/remote/RemoteApiServices.kt
+++ app/src/main/java/com/example/heroes_fight/data/domain/repository/remote/RemoteApiServices.kt
@@ -1,4 +1,45 @@
 package com.example.heroes_fight.data.domain.repository.remote
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface RemoteApiServices {
+
+    @GET("search/{nameToFind}")
+    suspend fun searchHeroByName(
+        @Path("nameToFind") nameToFind: String
+    ): Response<Any>
+
+    @GET("{idHero}")
+    suspend fun getHeroById(
+        @Path("idHero") idHero: Int
+    ): Response<Any>
+
+    @GET("{idHero}/powerstats")
+    suspend fun getHeroStatsById(
+        @Path("idHero") idHero: Int
+    ): Response<Any>
+
+    @GET("{idHero}/biography")
+    suspend fun getHeroBiographyById(
+        @Path("idHero") idHero: Int
+    ): Response<Any>
+
+
+    @GET("{idHero}/appearance")
+    suspend fun getHeroAppearanceById(
+        @Path("idHero") idHero: Int
+    ): Response<Any>
+
+    @GET("{idHero}/connections")
+    suspend fun getHeroConnectionsById(
+        @Path("idHero") idHero: Int
+    ): Response<Any>
+
+    @GET("{idHero}/image")
+    suspend fun getHeroImgById(
+        @Path("idHero") idHero: Int
+    ): Response<Any>
+
 }
