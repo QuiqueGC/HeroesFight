e1b9dac87533ec562a6e3d24d8de995b522bc154
Quique <enriquegcameselle@gmail.com>
2024-05-16 23:42:51 +0200

    FightFragment prepared to start design

---
+++ app/src/main/java/com/example/heroes_fight/ui/fight_fragment/FightFragment.kt
@@ -0,0 +1,22 @@
+package com.example.heroes_fight.ui.fight_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.heroes_fight.databinding.FragmentFightBinding
+
+class FightFragment : Fragment() {
+
+    private lateinit var binding: FragmentFightBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentFightBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+}
