e971fe4b1b862f390e18df5cc62b048bbf59cfa0
Quique <enriquegcameselle@gmail.com>
2024-05-19 16:35:22 +0200

    added attack functions to Fighter and HeroModel

--- app/src/main/java/com/example/heroes_fight/data/domain/model/Fighter.kt
+++ app/src/main/java/com/example/heroes_fight/data/domain/model/Fighter.kt
@@ -1,8 +1,13 @@
 package com.example.heroes_fight.data.domain.model
 
 import com.example.heroes_fight.data.domain.model.common.Position
+import com.example.heroes_fight.data.domain.model.hero.HeroModel
 
 interface Fighter {
 
     fun move(destinationPosition: Position): Boolean
+
+    fun attack(enemy: HeroModel): String
+
+    fun defenceRoll(): Int
 }
--- app/src/main/java/com/example/heroes_fight/data/domain/model/hero/HeroModel.kt
+++ app/src/main/java/com/example/heroes_fight/data/domain/model/hero/HeroModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.example.heroes_fight.data.domain.model.BaseModel
 import com.example.heroes_fight.data.domain.model.Fighter
 import com.example.heroes_fight.data.domain.model.common.Position
+import kotlin.random.Random
 
 data class HeroModel(
     val id: Int = 0,
@@ -20,17 +21,18 @@ data class HeroModel(
     var position: Position = Position()
 
 ) : BaseModel(), Fighter {
+
     override fun move(destinationPosition: Position): Boolean {
         Log.i("quique", "NOMBRE DEL FIGHTER ---> ${name}")
         var canMove = false
         val originValue = position.y + position.x
-        var destinationValue = destinationPosition.y + destinationPosition.x
+        val destinationValue = destinationPosition.y + destinationPosition.x
         val movementCapacity = if (speed / 10 < 1) {
             1
         } else {
             speed / 10
         }
-        
+
         Log.i("quique", "Posición destino (suma)---> ${destinationValue}")
         Log.i("quique", "Posición origen (suma)---> ${originValue}")
         Log.i("quique", "Speed ---> ${speed}")
@@ -64,5 +66,77 @@ data class HeroModel(
             }
         }
         return canMove
+    }
+
+
+    override fun attack(enemy: HeroModel): String {
+        val heroPositionValue = position.y + position.x
+        val enemyPositionValue = enemy.position.y + enemy.position.x
+        val result: String
+
+        if (heroPositionValue < enemyPositionValue) {
+            result = if (enemyPositionValue - heroPositionValue == 1) {
+                resolveAttack(enemy)
+
+            } else {
+                "${enemy.name} is so far"
+            }
+
+        } else {
+
+            result = if (heroPositionValue - enemyPositionValue == 1) {
+
+                resolveAttack(enemy)
+
+            } else {
+                "${enemy.name} is so far"
+            }
+        }
+        return result
+    }
+
+    private fun resolveAttack(enemy: HeroModel): String {
+        val attackRoll = Random.nextInt(1, 101)
+        val result: String
+
+        if (attackRoll <= combat) {
+            val attackDifference = combat - attackRoll
+            val enemyDefenceDifference = enemy.defenceRoll()
+            if (attackDifference >= enemyDefenceDifference) {
+
+                result = resolveDamage(attackDifference - enemyDefenceDifference, enemy)
+
+            } else {
+                result = "${enemy.name} defended the attack"
+            }
+        } else {
+            result = "$name failed the attack"
+        }
+
+        return result
+    }
+
+    private fun resolveDamage(damageBonus: Int, enemy: HeroModel): String {
+        val damageRoll = Random.nextInt(1, 101)
+        val result: String
+        if (damageRoll <= strength) {
+            val damage = strength - damageRoll + damageBonus
+            enemy.durability -= damage
+            result = "$name inflicted $damage of damage to ${enemy.name}"
+
+        } else {
+            result = "$name didn't used strength enough"
+        }
+        return result
+    }
+
+
+    override fun defenceRoll(): Int {
+        val defenceRoll = Random.nextInt(1, 101)
+        return if (defenceRoll < combat) {
+            combat - defenceRoll
+        } else {
+            0
+        }
     }
 }
