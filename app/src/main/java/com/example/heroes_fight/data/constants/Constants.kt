eee47404b08c740c27a5e7cf2e40a3436c261aa0
Quique <enriquegcameselle@gmail.com>
2024-05-22 13:50:54 +0200

    changed the access token

--- app/src/main/java/com/example/heroes_fight/data/constants/Constants.kt
+++ app/src/main/java/com/example/heroes_fight/data/constants/Constants.kt
@@ -2,5 +2,5 @@ package com.example.heroes_fight.data.constants
 
 object Constants {
 
-    const val BASE_URL = "https://superheroapi.com/api/10229233666327556/"
+    const val BASE_URL = "https://superheroapi.com/api/53a54684d68e10fb0f135e238913d5f9/"
 }
