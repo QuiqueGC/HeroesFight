166cdd9132d0c84b4edf02dcaec20a2c295f0a11
Quique <enriquegcameselle@gmail.com>
2024-07-02 15:05:01 +0200

    changed the connection dialog for a custom one

--- app/src/main/java/com/example/heroes_fight/ui/mode_selection_fragment/ModeSelectionFragment.kt
+++ app/src/main/java/com/example/heroes_fight/ui/mode_selection_fragment/ModeSelectionFragment.kt
@@ -1,13 +1,13 @@
 package com.example.heroes_fight.ui.mode_selection_fragment
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.heroes_fight.databinding.DialogHostingBinding
 import com.example.heroes_fight.databinding.FragmentModeSelectionBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -34,17 +34,17 @@ class ModeSelectionFragment : Fragment() {
             }
 
             btnTcpIp.setOnClickListener {
-                showHostDialog()
+                showCustomDialog()
             }
         }
     }
 
-    private fun showHostDialog() {
-        val builder = AlertDialog.Builder(requireContext())
-        builder.setTitle("Hosting")
-        builder.setMessage("¿Are you going to be the host?")
+    private fun showCustomDialog() {
+        val dialogBinding = DialogHostingBinding.inflate(layoutInflater)
+        val dialog = Dialog(requireContext())
+        dialog.setContentView(dialogBinding.root)
 
-        builder.setPositiveButton("Yes") { dialog, _ ->
+        dialogBinding.btnHost.setOnClickListener {
             findNavController().navigate(
                 ModeSelectionFragmentDirections.actionModeSelectionFragmentToFightP2PFragment(
                     true,
@@ -54,37 +54,22 @@ class ModeSelectionFragment : Fragment() {
             dialog.dismiss()
         }
 
-        builder.setNegativeButton("No") { dialog, _ ->
-            showIpDialog()
-            dialog.dismiss()
+        dialogBinding.btnClient.setOnClickListener {
+            dialogBinding.etIpAddress.visibility = View.VISIBLE
+            dialogBinding.btnConnectClient.visibility = View.VISIBLE
         }
 
-        builder.create().show()
-    }
+        dialogBinding.btnConnectClient.setOnClickListener {
 
-    private fun showIpDialog() {
-        val inputEditText = EditText(requireContext())
-
-        val dialogBuilder = AlertDialog.Builder(requireContext())
-            .setTitle("IP Address")
-            .setMessage("Please enter the IP address:")
-            .setView(inputEditText)
-            .setPositiveButton("OK") { dialog, _ ->
-                val ipAddress = inputEditText.text.toString()
-
-                findNavController().navigate(
-                    ModeSelectionFragmentDirections.actionModeSelectionFragmentToFightP2PFragment(
-                        false,
-                        ipAddress
-                    )
+            val ipAddress = dialogBinding.etIpAddress.text.toString()
+            findNavController().navigate(
+                ModeSelectionFragmentDirections.actionModeSelectionFragmentToFightP2PFragment(
+                    false,
+                    ipAddress
                 )
-                dialog.dismiss()
-            }
-            .setNegativeButton("Cancel") { dialog, _ ->
-                dialog.cancel()
-            }
-
-        val alertDialog = dialogBuilder.create()
-        alertDialog.show()
+            )
+            dialog.dismiss()
+        }
+        dialog.show()
     }
 }
